eb33df9adb936f0dd4b71f5e600b2eaa8f006463
Ayşe Nur Güven <guven_aysenur@hotmail.com>
2021-02-13 16:22:22 +0300

    Add project files.

---
+++ ConsoleApp1/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName+"  Eklendi." );
+        }
+            
+        public void List(Customer customer)
+        {
+
+            Console.WriteLine(customer.CustomerName + "  Listelendi.");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + "  Listelendi.");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Buğra";
+            customer1.CustomerSurname = "Kömürcü";
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+        }
+
+    }
+}
